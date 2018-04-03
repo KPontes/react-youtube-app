18dafe59c89a6030793dd484b5bf86bf2c556b57
Krishnan Pontes <krishnanpontes@gmail.com>
2018-04-03 11:10:17 -0300

    youtube app commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+//Class component have props available everywhere and must be used when you need to keep state
+class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {term: ''}; //constructor is the only place where you assgin state directly
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+	// Mandatory render method
+	render() {
+		return (
+			<div className="search-bar">
+				<input 
+					value = {this.state.term}
+					onChange={event => this.onInputChange(event.target.value)}/>
+			</div>
+		);
+	}
+
+	onInputChange(term) {
+		this.setState({term});
+		this.props.onSearchTermChange(term);
+	}
+
+}
+
+/* Function component
+const SearchBar = () => {
+	return <input />;
+};
+*/
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import _ from 'lodash';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyAtOLtlvila_Sd7B6_xLnSaWePXvCbUdMg';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+
+//create a new component thatt produces some html
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		//constructor is the only place where you assgin state directly	
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		}; 
+		this.videoSearch('surfboards');
+	}
+
+	videoSearch(term) {		
+		YTSearch({key: API_KEY, term: term}, (data) => {
+			this.setState({
+				videos: data,
+				selectedVideo: data[0]
+			});
+		});	
+	}
+
+	render() {
+		//returns a version of the function that runs only at every 1 second and not at each keystrike
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 2000);
+
+		return (
+			<div> 
+				<SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos}/>
+			</div>
+		);
+	}
+}
+
+// take the components generated html and puts it into the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
